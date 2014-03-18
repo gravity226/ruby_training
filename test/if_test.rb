b95a29a3e06d01db388839f44f52972af418ca12
Tommy Martin <gravity226@gmail.com>
2014-03-18 11:47:45 -0400

    more ifs

--- test/if_test.rb
+++ test/if_test.rb
@@ -1,10 +1,14 @@
-a = 25
+a = 22
 b = 23
 
 tf1 = a != b && a > b
 
 tf2 = a <= b || a % b > 2
 
+tf3 = if a - b > 0 then "a is greater than b" elsif a == b then "a equals b" else "a is less than b" end
+
 puts tf1 ? "The first expression (tf1) is True" : "The first expression (tf1) is False"
 puts ''
 puts tf2 ? "The second expression (tf2) is True" : "The second expression (tf2) is False"
+puts ''
+puts tf3
