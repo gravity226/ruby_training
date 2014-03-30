5570a1fadb2ef90c481ed5064f38052834c794f7
Tommy Martin <gravity226@gmail.com>
2014-03-30 16:13:13 -0400

    A little further.

--- rspec_tutorial/library_project/spec/library_spec.rb
+++ rspec_tutorial/library_project/spec/library_spec.rb
@@ -1,1 +1,24 @@
 require 'spec_helper'
+
+describe "Library Object" do
+
+  before :all do
+    lib_arr = [
+        Book.new("RSpec", "Someone", :development),
+        Book.new("Java", "Tommy", :development),
+        Book.new("iOS", "Martin", :development),
+        Book.new("Wool", "Author", :thriller),
+        Book.new("Learning to Fly", "Steph Davis", :autobiography)
+    ]
+
+    File.open "Books.yml", "w" do |f|
+      f.write YAML::dump lib_obj
+    end
+
+  end
+
+  before :each do
+    @library = Library.new "library.yml"
+  end
+
+end
